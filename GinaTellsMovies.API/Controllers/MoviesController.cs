b4ae07720132006786585309fe5f6959dd9402c9
JoaoMoraesJr <joaojr1997@gmail.com>
2021-04-08 20:04:33 -0300

    Initial commit

---
+++ GinaTellsMovies.API/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using GinaTellsMovies.API.Properties;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static GinaTellsMovies.API.Properties.ConfigApi;
+
+namespace GinaTellsMovies.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class MoviesController : ControllerBase
+    {
+
+        public class Movie
+        {
+            public string Name { get; set; }
+        }
+
+        private readonly HttpClient _client = new HttpClient();
+
+        [HttpGet]
+        public async Task<IActionResult> GetMovie()
+        {
+            Movie movie = null;
+            string movieString = "";
+            object movieJson = null;
+            const string api_key = ApiToken;
+            HttpResponseMessage response = await _client.GetAsync($"https://api.themoviedb.org/3/movie/popular?api_key={api_key}");
+            if (response.IsSuccessStatusCode)
+            {
+                movieString = await response.Content.ReadAsStringAsync();
+                //movieJson = JsonConvert.DeserializeObject(movieString);
+                movieJson = JToken.Parse(movieString).ToObject<object>();
+            }
+            return Ok(movieJson);
+        }
+    }
+}
