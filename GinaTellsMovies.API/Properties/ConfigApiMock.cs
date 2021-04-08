c1591df7b5dec0ad871d98b96da6ff8261c08e8f
JoaoMoraesJr <joaojr1997@gmail.com>
2021-04-08 20:57:00 -0300

    Project config

---
+++ GinaTellsMovies.API/Properties/ConfigApiMock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Delete the word Mock from this file/class and add IMDB Api Token to run the application
+
+namespace GinaTellsMovies.API.Properties
+{
+    public static class ConfigApiMock
+    {
+        public const string ApiToken = "ApiToken";
+    }
+}
