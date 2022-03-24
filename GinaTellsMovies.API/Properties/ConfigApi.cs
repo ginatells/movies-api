c0987c083fe21757d989f3b410e36fc17af05b7c
israel-deorce <israel.deorce@hp.com>
2022-03-24 20:16:00 -0300

    Added back configapi file

---
+++ GinaTellsMovies.API/Properties/ConfigApi.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GinaTellsMovies.API.Properties
+{
+    public static class ConfigApi
+    {
+        public const string ApiToken = "01d13f4240cdd91be8dac44716c4cc71";
+    }
+}
